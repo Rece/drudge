e4b00fc64da81ba5ae5c7ab13115c57057792a8c
Rece <PhilipReasa@gmail.com>
2015-10-25 00:19:33 -0700

    hooked up siteContent parser :)

--- app.js
+++ app.js
@@ -8,15 +8,18 @@ app.controller('main', [
 	'$http',
 	'$sce',
 	function($scope, $http, $sce){
-		$http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(rss))
-		.success(function(data){
-			$scope.stories = data.responseData.feed.entries;
-			$scope.loadStory(data.responseData.feed.entries[0]); //set the first story to active
-		});
+		$http.jsonp('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(rss))
+			.success(function(data){
+				$scope.stories = data.responseData.feed.entries;
+				$scope.loadStory(data.responseData.feed.entries[0]); //set the first story to active
+			});
 		
 		$scope.loadStory = function(story) {
 			$scope.active = story;
-			$scope.iframeLink = $sce.trustAsResourceUrl($scope.active.link);
+			$http.jsonp('http://localhost:3000/?callback=JSON_CALLBACK&url=' + encodeURIComponent($scope.active.link))
+				.success(function(data) {
+					$scope.storyText = $sce.trustAsHtml(data.html);
+				});
 		};
 	}
 ]);
